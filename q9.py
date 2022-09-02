8b3df138d14c79ed3cb31bcc67bc6d81ea6bab9d
sravaniseelam04 <111816262+sravaniseelam04@users.noreply.github.com>
2022-09-01 20:09:55 -0500

    change

--- q9.py
+++ q9.py
@@ -2,7 +2,7 @@ import math
 number = int(input("number of students:"))
 A = []
 B = []
-for i  in range(number):
+for i in range(number):
     A.append(int(input()))
 for b in A:
     a = (math.floor((b/2.2046)*100))/100;
